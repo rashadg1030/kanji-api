65448858acb3a880cfc78e7b956fffc6deec3702
rashadg1030 <rashad.sasaki@gmail.com>
2019-01-21 12:47:18 -0800

    Adding postgresql database.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
 {-# LANGUAGE DeriveGeneric #-}
+
+-- For Database
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Lib (app) where
 
@@ -10,19 +20,23 @@ import Data.Aeson hiding (json)
 import Data.Monoid ((<>))
 import Data.Text (Text, pack)
 import GHC.Generics
-import Text.XML.HXT.Core hiding (app)
-import Text.Pretty.Simple (pPrint) 
 import Control.Monad.IO.Class
 
+-- For Database
+import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
+import           Database.Persist        hiding (get) -- To avoid a naming clash with Web.Spock.get
+import qualified Database.Persist        as P         -- We'll be using P.get later for GET /people/<id>.
+import           Database.Persist.Postgresql hiding (get)
+import           Database.Persist.TH
 -- Kanji Data Type --
 data Kanji = Kanji {
     literal :: Text, -- literal character
     grade :: Int, -- grade of the kanji
     strokes :: Int, -- strokes needed to write kanji
-    jaOn :: [Text], -- "onyomi" of the kanji
-    jaKun :: [Text], -- "kunyomi" of the kanji
-    def :: [Text], -- definitions of the kanji
-    nanori :: [Text] -- readings of the kanji used in names
+    jaOn :: Text, -- "onyomi" of the kanji
+    jaKun :: Text, -- "kunyomi" of the kanji
+    def :: Text, -- definitions of the kanji
+    nanori :: Text -- readings of the kanji used in names
 } deriving (Generic, Show)
 
 instance ToJSON Kanji
@@ -36,6 +50,5 @@ type ApiAction a = SpockAction () () () a
 app :: Api
 app = do
         get "kanji" $ do
-           t <- liftIO runParser
-           json . show $ t
+           json . show $ "kanji"
 
